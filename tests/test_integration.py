aaeebe81c0dd842f1a3c9cded19382fac6d65b50
Harsh Doharey <bennyharsh@gmail.com>
2025-12-01 04:10:22 +0530

    test_integration.py

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,9 @@
+import sys
+sys.path.append(".")
+
 import json
 from app.main import app
+
 
 def test_health_endpoint():
     client = app.test_client()
@@ -7,6 +11,7 @@ def test_health_endpoint():
     assert response.status_code == 200
     data = json.loads(response.data.decode())
     assert data.get("status") == "ok"
+
 
 def test_add_endpoint():
     client = app.test_client()
